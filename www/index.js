eab99ed515fc27090e3e9a49520c465441ec0ebe
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2019-06-18 11:20:20 +0900

    Add weight between ticks

--- www/index.js
+++ www/index.js
@@ -3,10 +3,13 @@ import { Universe } from "wasm-game-of-life";
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
 
+let renderCount = 0;
 const renderLoop = () => {
-  pre.textContent = universe.render();
-  universe.tick();
-
+  if (renderCount % 6 == 0) {
+    pre.textContent = universe.render();
+    universe.tick();
+  }
+  renderCount += 1;
   requestAnimationFrame(renderLoop);
 };
 
